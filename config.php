725a75c06f9cdd1fa67337e03202ee6746d29a8e
olezhek85 <olezhek_85@list.ru>
2016-12-25 18:42:20 +0200

    forms, request types and routing

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'name' => 'mytodo',
         'username' => 'root',
-        'password' => '',
+        'password' => 'secret',
         'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
